28339b6096e3c1061c5eb3e5d5e228bb11e396d3
hoon-ock <jock@andrew.cmu.edu>
2024-05-08 16:00:40 -0400

    name changes

--- ssir_modified_muller_LD.py
+++ ssir_modified_muller_LD.py
@@ -5,14 +5,9 @@ import matplotlib.pyplot as plt
 from matplotlib.pyplot import cm
 import numpy as np
 
-from energy_landscape import MullerForce, ModifiedMullerForce, landscape
-from muller_utils import (ObsBoundReInit, 
-                          random_init_point, 
-                          random_init_point_with_grid,
+from energy_landscape import landscape
+from muller_utils import (random_init_point_with_grid,
                           get_grid_intersection_points,
-                          target_init_point, 
-                          find_region_change, 
-                          deepest_well_escape_frame, 
                           success_cluster_identification)
 
 import os, tqdm, pickle, yaml
@@ -24,7 +19,7 @@ argparser.add_argument('--interval', type=float, default=1.5)
 argparser.add_argument('--num_step', type=int, default=2000)
 argparser.add_argument('--seed', type=int, default=1)
 
-parent_dir = os.path.join( 'results/mod_muller/ogn', argparser.parse_args().dir)
+parent_dir = os.path.join('results/mod_muller/LD', argparser.parse_args().dir)
 interval = argparser.parse_args().interval
 num_step = argparser.parse_args().num_step
 seed = argparser.parse_args().seed
@@ -51,19 +46,14 @@ ft = 18
 e = 0.2
 # Observation Guided Navigation parameters
 np.random.seed(seed)
-total_step = num_step       # total number of steps
-large_batch_size = params['large_batch_size']  # number of steps for outer loop
-small_batch_size = params['small_batch_size']   # number of steps for inner loop (scout)
-k = params['k']                # update rate parameter (higher k means smaller update)
-boundary_frac = params['boundary_frac']     # threshold to determine bounday based on the fraction of points in the region
-
+total_step = num_step #2000        # total number of steps
 
 # Brownian simulation parameters
 ## each particle is totally independent, propagating under the same potential
 nParticles = params['nParticles']
 temp_non_dimensional = params['temperature']
 mass_non_dimensional = params['mass']
-friction_non_dimensional = params['friction']
+friction_non_dimensional = params['friction']   
 timestep_non_dimensional = params['timestep']
 step = params['step']
 temperature = temp_non_dimensional * kelvin
@@ -76,7 +66,6 @@ timestep = timestep_non_dimensional * femtosecond
 # Get initial points on the grid
 _, grid_x, grid_y = random_init_point_with_grid(x_limits_grid, y_limits_grid, grid_interval=grid_interval)
 initial_points = get_grid_intersection_points(grid_x, grid_y)
-
 
 # Brownian simulation set-up
 system = mm.System()
@@ -94,104 +83,32 @@ context = mm.Context(system, integrator)
 init_no = 1
 total_num_success = 0
 total_num_clusters = 0
-valid_init = [] # valid initial points with their numer of sucess identifications
-# breakpoint()
+valid_init = []
+#breakpoint()
 for init in tqdm.tqdm(initial_points):
-    # print(f'============ Iteration Number: {init_no} ============')
-    # Preparation to run simulations
-    traj_all_save = []
-    # breakpoint()
     init = init.reshape(1, 3)
-    cent = init[:,:2].T 
-    v = 0
     startingPositions = init
-    #########################
-    # Start simulation
-    remain_step = total_step
-    n=1 # outer loop counter
-    while remain_step > 0:
-        # Initialization    
-        context.setPositions(startingPositions)
-        context.setVelocitiesToTemperature(temperature)
 
-        # Outer iteration (Run Browninan Simulation)
-        traj = np.zeros((large_batch_size,3))
-        for i in range(large_batch_size):
-            x = context.getState(getPositions=True).getPositions(asNumpy=True).value_in_unit(nanometer)   
-            p = x[0]
-            E = pes.potential(x=p[0], y=p[1])
-            traj[i] = np.array([p[0], p[1], E])
-            integrator.step(step)
-        traj_all_save.append(traj)
+    context.setPositions(startingPositions)
+    context.setVelocitiesToTemperature(temperature)
 
-        # Instantiation
-        particle_outer = ObsBoundReInit(traj, cent_prev=cent, inc_prev=v,
-                                        delta=0.0001, k=k)
+    traj = np.zeros((total_step, 3))
+    for i in range(total_step):
+        # Assuming context, pes, and integrator are predefined and properly set up
+        x = context.getState(getPositions=True).getPositions(asNumpy=True).value_in_unit(nanometer)   
+        p = x[0]
+        E = pes.potential(x=p[0], y=p[1])  # Ensure pes.potential() handles inputs safely and returns a sensible value
 
-        # Secure boundary & centroid & last point & kernel (density gradient)
-        # to determine whether the next centroid is inside the boundary
-        boundary_path = particle_outer.get_contour_path(target_fraction=boundary_frac)
-        kde_outer = particle_outer.kde()
-
-        cent = particle_outer.cent
-        last = particle_outer.last
-        remain_step -= large_batch_size # step counter
-
-        # Decision block 
-        ## Determine whether the centroid inside any boundary paths 
-        ## Start inner loop after the decision
-        ## If the centroid is outside all boundary paths, this while loop would be skipped!
-        m=0 # inner loop counter
-        while (particle_outer.increase(boundary_path, cent) != 0) and (remain_step > 0):
-            # Instantiation
-            particle_inner = ObsBoundReInit(traj, cent_prev=cent, inc_prev=v,
-                                            delta=0.0001, k=k)
+        traj[i] = np.array([p[0], p[1], E])
+        integrator.step(step)
     
-            # Update reinitialization point
-            ## Update rate is amplified by the number of times the centroid is revisited
-            density_grad = particle_inner.density_gradient(point=last, kde=kde_outer)
-            p_new, upd = particle_inner.update_position(grad=density_grad, vn=v) 
-            E_new = pes.potential(*p_new)
-            startingPositions = np.vstack((p_new, E_new)).T
-
-            # Re-Initialization
-            context.setPositions(startingPositions)
-            context.setVelocitiesToTemperature(temperature)       
-            # Inner iteration (Run Brownian Simulation)
-            traj = np.zeros((small_batch_size,3))
-            for i in range(small_batch_size):
-                x = context.getState(getPositions=True).getPositions(asNumpy=True).value_in_unit(nanometer)   
-                p = x[0]
-                E = pes.potential(x=p[0], y=p[1])
-                traj[i] = np.array([p[0], p[1], E]) 
-                integrator.step(step)
-            traj_all_save.append(traj)
-
-            # Secure centroid & last point & v_n for the next run
-            v += 1.0      
-            cent = particle_inner.cent
-            last = particle_inner.last    
-            remain_step -= small_batch_size
-            m+=1
-            if remain_step < 0:
-                print('reached the max step')
-                break
-
-        # Reset v_n & init point 
-        ## init point as last traj from newly found cluster 
-        v=0 # update rate reset 
-        p_new = last # last point as the new initial point
-        E_new = pes.potential(*p_new)
-        startingPositions = np.vstack((p_new, E_new)).T
-        n+=1
-    
-
-    traj_all = np.vstack(traj_all_save)
+    traj_all = traj #np.vstack(traj_all_save)
     # success identification of the cluster
     # count number of data points within x_limit and y_limit
     data_count = len(traj_all[(traj_all[:,0] > x_limits[0]) & (traj_all[:,0] < x_limits[1]) \
                         & (traj_all[:,1] > y_limits[0]) & (traj_all[:,1] < y_limits[1])])
-    if data_count > num_step/10:
+    if data_count > num_step/2:
+        mean_x = np.mean(traj_all[:,0])
         num_identified_cluster = success_cluster_identification(traj_all[:,:2], 
                                                            center_points, 
                                                            distance_threshold=0.1, 
@@ -202,7 +119,6 @@ for init in tqdm.tqdm(initial_points):
         valid_init.append(init)
         plot_save_file = f'{init_no}_{num_identified_cluster}.png'
     else:
-        print(f'failed to identify the cluster: {init_no}')
         plot_save_file = f'{init_no}_0_fail.png'
     # save plot
     plt.close()
@@ -247,16 +163,15 @@ pes.plot(ax=plt.gca(),
          miny=y_limits[0]-e, maxy=y_limits[1]+e,
          fontsize=ft-4)
 
+
 for x in grid_x:
     plt.axvline(x, color='lightgray', linestyle='--')
 
 # Plot horizontal lines for each y_grid point
 for y in grid_y:
     plt.axhline(y, color='lightgray', linestyle='--')
-# breakpoint()
 
 valid_initial_points = np.vstack(valid_init) #np.array(valid_init)
-
 
 # Define colors for each value
 colors = {1: 'red', 2: 'orange', 3: 'green'}
